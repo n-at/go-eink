f1450019063b5653962d5a3a6ded0c3b20e64ca0
Alexey Nurgaliev <atnurgaliev@gmail.com>
2022-12-23 23:19:12 +0300

    flags and readme

--- README.md
+++ README.md
@@ -1,8 +1,41 @@
-# eink
+# image to e-ink
 
-Output image on GoodDisplay IL075U e-ink display
+Output an arbitrary image on [GoodDisplay IL075U](https://www.good-display.com/product/404.html) e-ink display.
 
-## Linux permissions
+Tested on macOS Ventura (amd64). 
+
+## Build
+
+Go 1.19+ required.
+
+```bash
+go build -a -o app
+```
+
+## Usage
+
+Run with `-help` flag to get all options:
+
+```txt
+-device string
+    device name, required, can be obtained with -list flag
+-image string
+    path to image to print, required
+-image-align string
+    image alignment, one of: top-left, top-middle, top-right, middle-left, middle, middle-right, bottom-left, bottom-middle, bottom-right (default "middle")
+-image-dithering-algo string
+    dithering algorithm, one of: floyd_steinberg, jarvis_judice_ninke, atkinson, burkes, stucki, sierra (default "floyd_steinberg")
+-image-dithering-threshold int
+    dithering threshold, 0..256 (default 128)
+-image-enlarge
+    enlarge image to fit screen
+-list
+    show available devices and exit
+-verbose
+    show extended output
+```
+
+## Linux USB permissions
 
 ```bash
 sudo nano /etc/udev/rules.d/50-myusb.rules
@@ -15,6 +48,10 @@ SUBSYSTEMS=="usb", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7523", GROUP="use
 ```
 
 `idVendor` and `idProduct` can be found in output of `lsusb -vvv`.
-Or run program without parameters to list available ports with USB device info.
+Or run program with `-list` flag to list available ports with USB device info.
 
 Reboot machine.
+
+## Troubleshooting
+
+If image is not shown, try to reconnect device and run program again (it happens randomly with no visible reasons).
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	log "github.com/sirupsen/logrus"
 	"go-eink/eink"
 	"go-eink/images"
@@ -8,29 +9,51 @@ import (
 )
 
 func main() {
+	verbose := flag.Bool("verbose", false, "show extended output")
+	list := flag.Bool("list", false, "show available devices and exit")
+	deviceName := flag.String("device", "", "device name, required, can be obtained with -list flag")
+	imagePath := flag.String("image", "", "path to image to print, required")
+	imageEnlarge := flag.Bool("image-enlarge", false, "enlarge image to fit screen")
+	imageAlign := flag.String("image-align", "middle", "image alignment, one of: top-left, top-middle, top-right, middle-left, middle, middle-right, bottom-left, bottom-middle, bottom-right")
+	imageDitheringAlgorithm := flag.String("image-dithering-algo", "floyd_steinberg", "dithering algorithm, one of: floyd_steinberg, jarvis_judice_ninke, atkinson, burkes, stucki, sierra")
+	imageDitheringThreshold := flag.Int("image-dithering-threshold", 128, "dithering threshold, 0..256")
+	flag.Parse()
+
+	//prepare logger
 	log.SetFormatter(&log.TextFormatter{
 		FullTimestamp: true,
 	})
 	log.SetOutput(os.Stdout)
-	log.SetLevel(log.DebugLevel)
-	//log.SetLevel(log.InfoLevel)
+	if *verbose {
+		log.SetLevel(log.DebugLevel)
+	} else {
+		log.SetLevel(log.InfoLevel)
+	}
 
-	img, err := images.Open("./assets/img.jpg")
+	//list devices
+	if *list {
+		eink.EnumerateDevicesExtended()
+		return
+	}
+
+	//prepare imagePath
+	if len(*imagePath) == 0 {
+		log.Fatal("image required")
+	}
+	img, err := images.Open(*imagePath)
 	if err != nil {
 		log.Fatalf("unable to open image: %s", err)
 	}
-
-	img = images.Resize(img, eink.ImageWidth, eink.ImageHeight, false)
-	img = images.Align(img, eink.ImageWidth, eink.ImageHeight, images.AlignMiddle)
-	img = images.Dithering(img, images.DitheringFloydSteinberg, 128)
+	img = images.Resize(img, eink.ImageWidth, eink.ImageHeight, *imageEnlarge)
+	img = images.Align(img, eink.ImageWidth, eink.ImageHeight, images.GetAlign(*imageAlign))
+	img = images.Dithering(img, images.GetDitheringAlgorithm(*imageDitheringAlgorithm), *imageDitheringThreshold)
 	imageData := images.ToImageData(img)
 
-	eink.EnumerateDevicesExtended()
-	if err := eink.Print("/dev/cu.usbserial-14140", imageData); err != nil {
+	//print
+	if len(*deviceName) == 0 {
+		log.Fatal("device required")
+	}
+	if err := eink.Print(*deviceName, imageData); err != nil {
 		log.Errorf("unable to print image: %s", err)
 	}
-
-	//if err := images.Save(img, "./assets/output.png"); err != nil {
-	//	log.Fatalf("unable to save image: %s", err)
-	//}
 }
