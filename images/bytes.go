3f3dca14718f85cde4b037b3eb8a19c73fe58885
Alexey Nurgaliev <atnurgaliev@gmail.com>
2025-05-31 12:53:29 +0300

    prepare output bytes for BWRY

--- images/bytes.go
+++ images/bytes.go
@@ -2,6 +2,13 @@ package images
 
 import "image"
 
+const (
+	BWRY_B = 0b00
+	BWRY_W = 0b01
+	BWRY_R = 0b11
+	BWRY_Y = 0b10
+)
+
 func ToImageData(img image.Image) []byte {
 	width := img.Bounds().Size().X
 	height := img.Bounds().Size().Y
@@ -37,5 +44,53 @@ func ToImageData(img image.Image) []byte {
 }
 
 func ToImageDataBWRY(blendMode BlendMode, imgBW, imgRW, imgYW image.Image) []byte {
-	return nil //TODO
+	width := imgBW.Bounds().Dx()
+	height := imgBW.Bounds().Dy()
+
+	outputLength := (width * height) / 4
+	output := make([]byte, outputLength)
+	outputIdx := 0
+
+	for y := 0; y < height; y++ {
+		for xStart := 0; xStart < width; xStart += 4 {
+			var val byte = 0
+			for xPos := 0; xPos < 4; xPos++ {
+				val <<= 2
+
+				x := xStart + xPos
+
+				black := false
+				red := false
+				yellow := false
+
+				if v, _, _, _ := imgBW.At(x, y).RGBA(); v == 0 {
+					black = true
+				}
+				if v, _, _, _ := imgRW.At(x, y).RGBA(); v == 0 {
+					red = true
+				}
+				if v, _, _, _ := imgYW.At(x, y).RGBA(); v == 0 {
+					yellow = true
+				}
+
+				c := BlendColors(blendMode, black, red, yellow)
+
+				switch c {
+				case BlendModeB:
+					val += BWRY_B
+				case BlendModeR:
+					val += BWRY_R
+				case BlendModeY:
+					val += BWRY_Y
+				default:
+					val += BWRY_W
+				}
+			}
+
+			output[outputIdx] = val
+			outputIdx++
+		}
+	}
+
+	return output
 }
